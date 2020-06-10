fabc7f88114c0ff579c6ad599154c44f9c2b05d5
Naman Kumar Garg <31842804+Naman-Garg-06@users.noreply.github.com>
2020-06-10 11:45:07 +0530

    Update settings.py
    added command for online hosting on any platform

--- intern/settings.py
+++ intern/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dgs)e8=@3@2(vaww^lp&7sz4vgg)k^!uw0lrj!v=7sihr)!e6p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
